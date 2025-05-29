f99ce28f61581f3fd09f90d190a9ffa5343fe37c
JeraldJF <jeraldj@sanketika.in>
2025-05-29 11:09:01 +0530

    feat #FP-I26: trimming of option correct answer

--- src/quiz_creation_script/index.ts
+++ src/quiz_creation_script/index.ts
@@ -93,7 +93,7 @@ async function processQuestionCsv() {
                             if (headers.includes(isCorrectKey)) {
                                 optionPairs.push({
                                     text: row[textKey],
-                                    isCorrect: String(row[isCorrectKey]).toLowerCase() === 'true'
+                                    isCorrect: String(row[isCorrectKey]).toLowerCase().trim() === 'true'
                                 });
                             }
                         }
