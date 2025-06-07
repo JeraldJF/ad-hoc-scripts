53076ba3c8b1521c6fc8ac19a1c1e990ebc687aa
JeraldJF <jeraldj@sanketika.in>
2025-06-07 11:25:54 +0530

    feat #FP-I52: support for batch enrollments

--- src/course_enrollment_script/config/config.ts
+++ src/course_enrollment_script/config/config.ts
@@ -7,5 +7,7 @@ export const config = {
     clientSecret: globalConfig.clientSecret || '',
     grant_type: globalConfig.grant_type || 'password',
     channelId: globalConfig.channelId || '01429195271738982411',
-    enrollUserWaitInterval: process.env.ENROLL_USER_WAIT_INTERVAL ? Number(process.env.ENROLL_USER_WAIT_INTERVAL) : 0
+    enrollUserWaitInterval: process.env.ENROLL_USER_WAIT_INTERVAL ? Number(process.env.ENROLL_USER_WAIT_INTERVAL) : 0,
+    enrollmentBatchSize: process.env.ENROLLMENT_BATCH_SIZE ? Number(process.env.ENROLLMENT_BATCH_SIZE) : 5,
+    courseBatchSize: process.env.COURSE_BATCH_SIZE ? Number(process.env.COURSE_BATCH_SIZE) : 1,
 }
